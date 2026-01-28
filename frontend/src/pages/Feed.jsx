1b1f4ac2994798fcced548d84ac72135aa29b061
AlonaHrytsenko <alenagrytsenko111@gmail.com>
2026-01-28 10:43:23 +0100

    feat:  feed end-state

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Post from '../components/Post'
 import './Feed.css'
+import endPicture from '../assets/end-icon.jpg'
 
 const Feed = ({ posts, onPostClick, onPostUpdate, onFollowUpdate }) => {
   const currentUserId = localStorage.getItem('userId')
@@ -9,18 +10,27 @@ const Feed = ({ posts, onPostClick, onPostUpdate, onFollowUpdate }) => {
     <div className="feed-container">
       <div className="feed-page">
         <main className="feed-grid">
-          {/* Проверяем, есть ли посты, чтобы не было ошибки при пустом массиве */}
           {posts && posts.length > 0 ? (
-            posts.map((post) => (
-              <Post
-                key={post._id} // Ключ лучше здесь
-                post={post}
-                currentUserId={currentUserId}
-                onPostClick={() => onPostClick(post)}
-                onPostUpdate={onPostUpdate}
-                onFollowUpdate={onFollowUpdate}
-              />
-            ))
+            <>
+              {posts.map((post) => (
+                <Post
+                  key={post._id}
+                  post={post}
+                  currentUserId={currentUserId}
+                  onPostClick={() => onPostClick(post)}
+                  onPostUpdate={onPostUpdate}
+                  onFollowUpdate={onFollowUpdate}
+                />
+              ))}
+
+              <div className="feed-end-message">
+                <img src={endPicture} alt="Checkmark" className="end-icon" />
+                <p className="end-title">You've seen all the updates</p>
+                <p className="end-subtitle">
+                  You have viewed all new publications
+                </p>
+              </div>
+            </>
           ) : (
             <p className="empty-msg">No posts yet.</p>
           )}
