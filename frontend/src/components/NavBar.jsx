1eb4eba1a80623e1be955cb7ccd6ef05abbbb53e
AlonaHrytsenko <alenagrytsenko111@gmail.com>
2026-02-10 11:52:33 +0100

    style: improve Sidebar navigation styling and layout

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -25,71 +25,72 @@ const NavBar = ({
 
   return (
     <aside className="sidebar">
-      <div className="logo-container">
-        <Link to="/" className="logo-link">
-          <img src={logo} alt="logo" />
-        </Link>
-      </div>
-      <nav className="nav-menu">
-        <Link to="/" className="nav-item">
-          <FaHome size={26} />
-          <span>Home</span>
-        </Link>
-
-               <div
-          className={`nav-item ${isSearchOpen ? 'active' : ''}`}
-          onClick={onSearchClick}
-        >
-          <FaSearch size={26} />
-          <span>Search</span>
+      <div className="sidebar-container">
+        <div className="logo-container">
+          <Link to="/" className="logo-link">
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
+        <nav className="nav-menu">
+          <Link to="/" className="nav-item">
+            <FaHome size={26} />
+            <span>Home</span>
+          </Link>
 
-        <Link to="/explore" className="nav-item">
-          <FaCompass size={26} />
-          <span>Explore</span>
-        </Link>
-
-        <Link to="/messages" className="nav-item">
-          <FaRegPaperPlane size={26} />
-          <span>Message</span>
-        </Link>
-
-      
-        <div
-          className={`nav-item ${isNotifOpen ? 'active' : ''}`}
-          onClick={onNotificationsClick}
-        >
-          <div className="icon-wrapper">
-            <FaRegHeart size={26} />
-            {unreadCount > 0 && (
-              <span className="notif-badge">{unreadCount}</span>
-            )}
+          <div
+            className={`nav-item ${isSearchOpen ? 'active' : ''}`}
+            onClick={onSearchClick}
+          >
+            <FaSearch size={26} />
+            <span>Search</span>
           </div>
-          <span>Notifications</span>
-        </div>
 
-        <div
-          className="nav-item"
-          onClick={onCreateClick}
-          style={{ cursor: 'pointer' }}
-        >
-          <FaPlusSquare size={26} />
-          <span>Create</span>
-        </div>
+          <Link to="/explore" className="nav-item">
+            <FaCompass size={26} />
+            <span>Explore</span>
+          </Link>
 
-        <Link
-          to={`/profile/${userId}`}
-          className="nav-item"
-          style={{ marginTop: 40 }}
-        >
-          {userAvatar ? (
-            <img src={userAvatar} alt="Profile" className="nav-avatar" />
-          ) : (
-            <FaUserCircle size={26} />
-          )}
-          <span>Profile</span>
-        </Link>
-      </nav>
+          <Link to="/messages" className="nav-item">
+            <FaRegPaperPlane size={26} />
+            <span>Message</span>
+          </Link>
+
+          <div
+            className={`nav-item ${isNotifOpen ? 'active' : ''}`}
+            onClick={onNotificationsClick}
+          >
+            <div className="icon-wrapper">
+              <FaRegHeart size={26} />
+              {unreadCount > 0 && (
+                <span className="notif-badge">{unreadCount}</span>
+              )}
+            </div>
+            <span>Notifications</span>
+          </div>
+
+          <div
+            className="nav-item"
+            onClick={onCreateClick}
+            style={{ cursor: 'pointer' }}
+          >
+            <FaPlusSquare size={26} />
+            <span>Create</span>
+          </div>
+
+          <Link
+            to={`/profile/${userId}`}
+            className="nav-item"
+            style={{ marginTop: 40 }}
+          >
+            {userAvatar ? (
+              <img src={userAvatar} alt="Profile" className="nav-avatar" />
+            ) : (
+              <FaUserCircle size={26} />
+            )}
+            <span>Profile</span>
+          </Link>
+        </nav>
+      </div>
     </aside>
   )
 }
